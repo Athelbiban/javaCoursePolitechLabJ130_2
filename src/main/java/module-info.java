2cf80a32bc2a90b1baa25ed57420da10833d99c0
athelbiban <vostrovso@gmail.com>
2024-01-22 12:05:09 +0300

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lab.javacoursepolitechlabj130_2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lab.javacoursepolitechlabj130_2 to javafx.fxml;
+    exports lab.javacoursepolitechlabj130_2;
+}
